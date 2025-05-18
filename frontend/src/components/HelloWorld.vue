d1347a62286e334b3e610f381f83f7f2f8f38a10
nikita2811 <39294680+nikita2811@users.noreply.github.com>
2025-05-18 13:09:23 +0530

    editor code

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,58 +1,57 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <div ref="container" class="monaco-container"></div>
+   <button @click="runCode" class="run-btn">Run</button>
+    <pre class="output">{{ output }}</pre>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+<script setup>
+/* global monaco */
+import { onMounted, ref } from 'vue'
+
+const container = ref(null)
+const output = ref('')
+let editor = null
+
+onMounted(() => {
+  if (!window.require) {
+    console.error('Monaco loader not available yet.')
+    return
+  }
+  // Load Monaco globally (assumes it's on window.require)
+  window.require.config({ paths: { vs: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.45.0/min/vs' } })
+   window.require(['vs/editor/editor.main'], () => {
+    editor = monaco.editor.create(container.value, {
+      value: `"Hello from Monaco!"`,
+      language: 'javascript,python',
+      theme: 'vs-dark',
+    })
+  })
+})
+function runCode() {
+  try {
+    const code = editor.getValue()
+    console.log('Running code:', code)
+    const capturedLogs = []
+
+    // Capture console.log
+    const originalLog = console.log
+    console.log = (...args) => capturedLogs.push(args.join(' '))
+
+    // Run the code
+    new Function(code)()
+
+    // Restore console.log and show output
+    console.log = originalLog
+    output.value = capturedLogs.join('\n') || '[No output]'
+  } catch (err) {
+    output.value = `Error: ${err.message}`
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.monaco-container {
+  height: 500px;
+  border: 1px solid #ccc;
 }
 </style>
